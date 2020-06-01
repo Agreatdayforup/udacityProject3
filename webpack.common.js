7a7209dad0c55a94b4d2016502acab2f270c647f
Patrick <cayerp@gmail.com>
2020-06-01 09:14:45 -0400

    error fixes

--- webpack.common.js
+++ webpack.common.js
@@ -14,7 +14,7 @@ module.exports = {
         module: {
             rules: [
                 {
-                    test: /\.js$/,
+                    test: /\.js?$/,
                     exclude: /node_modules/,
                     use: {
                         loader: "babel-loader"
@@ -37,13 +37,13 @@ module.exports = {
                 },
                 {
                     test: /\.s[ac]ss$/i,
-                    use: [
-                      // Creates `style` nodes from JS strings
-                      'style-loader',
-                      // Translates CSS into CommonJS
-                      'css-loader',
-                      // Compiles Sass to CSS
-                      'sass-loader',
+                        use: [
+                        // Creates `style` nodes from JS strings
+                        'style-loader',
+                        // Translates CSS into CommonJS
+                        'css-loader',
+                        // Compiles Sass to CSS
+                        'sass-loader',
                     ],
                   },
                 
