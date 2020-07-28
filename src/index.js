15f20826382be864b6bd48abe466645a63078b81
Patrick C <35810793+Agreatdayforup@users.noreply.github.com>
2020-07-28 15:07:00 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 console.log('the client side is loaded');
 
-import { handleSubmit } from './client/js/app';
+import { handleSubmit } from './client/js/app.js';
 
 //const handleSubmit = require('.src/client/js/app.js')
 import './client/styles/main.scss';
